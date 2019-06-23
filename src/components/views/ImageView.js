a325b347e65d05c33a7be2f3b1e93229f7a3680c
Ruslan <RRCFoxyLoxy@yandex.ru>
2019-06-24 02:55:47 +0300

    added fully working image view

--- src/components/views/ImageView.js
+++ src/components/views/ImageView.js
@@ -9,6 +9,7 @@ class ImageView extends React.Component {
     };
     this.setSelected = this.setSelected.bind(this);
     this.changeFull = this.changeFull.bind(this);
+    this.swipePicture = this.swipePicture.bind(this);
   }
 
   setSelected = (index) => {
@@ -29,12 +30,28 @@ class ImageView extends React.Component {
     }
   }
 
+  swipePicture = (event) => {
+    var activeIndex = this.state.selected;
+    if (activeIndex < this.props.content.length - 1){
+      activeIndex++;
+    } else {
+      activeIndex = 0;
+    }
+    this.setState({
+      selected: activeIndex,
+      fullSize: this.props.content[activeIndex]
+    });
+    event.stopPropagation();
+  }
+
   render(){
     return (
       <div>
         {this.state.fullSize ?
-        <div className='vi-dim' onClick={e => this.changeFull(false)}>
-          <div style={{backgroundImage: `url(${this.state.fullSize})`}}></div>
+        <div className="vi-overlay" onClick={e => this.changeFull(false)}>
+          <div className="vi-img-container">
+            <img className="vi-img-container vi-overlay-img" src={this.state.fullSize} onClick={e => this.swipePicture(e)}/>
+          </div>
         </div>: null}
         <div style={{backgroundImage: `url(${this.props.content[this.state.selected]})`}} onClick={e => this.changeFull(true)} className="vi-card-large vi-column vi-center-crop"></div>
         <div className='vi-flex-left vi-row' style={{paddingBottom:'15px'}}>
